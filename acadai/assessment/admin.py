2a4b6b76c216f0136e7a4baebb7180b51d8d49f4
Richard <rcezea007@gmail.com>
2026-01-04 20:25:02 +0100

    register Submission and Answer models

--- acadai/assessment/admin.py
+++ acadai/assessment/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Exam, Question
+from .models import Submission, Answer, Exam, Question
 
 # Register your models here.
-
+admin.site.register(Submission)
+admin.site.register(Answer)
 admin.site.register(Exam)
 admin.site.register(Question)
