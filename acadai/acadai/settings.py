72429154b3796e7a70b38a6ade3facec9328471a
Richard <rcezea007@gmail.com>
2026-01-04 20:38:23 +0100

    secure API with token-based authentication

--- acadai/acadai/settings.py
+++ acadai/acadai/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'assessment',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+SPECTACULAR_SETTINGS = {"TITLE": "Acad AI API"}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
--- acadai/assessment/urls.py
+++ acadai/assessment/urls.py
@@ -5,6 +5,7 @@ from .views import (
     SubmitExamAPIView,
     MySubmissionsAPIView,
 )
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("exams/<int:exam_id>/", ExamDetailAPIView.as_view()),
     path("exams/<int:exam_id>/submit/", SubmitExamAPIView.as_view()),
     path("my-submissions/", MySubmissionsAPIView.as_view()),
+    path('auth/token/', obtain_auth_token),
 ]
