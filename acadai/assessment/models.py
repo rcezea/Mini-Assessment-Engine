99785a38e8dd614608fe5a230cd50d8dabeac395
Richard <rcezea007@gmail.com>
2026-01-04 19:38:27 +0100

    define exam and question domain models

--- acadai/assessment/models.py
+++ acadai/assessment/models.py
@@ -1,2 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Exam(models.Model):
+    PUBLISHED = "PUBLISHED"
+    CLOSED = "CLOSED"
+
+    STATUS = [(PUBLISHED, "Published"), (CLOSED, "Closed")]
+
+    title = models.CharField(max_length=255)
+    course = models.CharField(max_length=100)
+    duration_minutes = models.IntegerField()
+    metadata = models.JSONField(blank=True, null=True)
+    status = models.TextField(max_length=10, choices=STATUS, default="DRAFT")
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    TEXT = "TEXT"
+    MCQ = "MCQ"
+
+    QUESTION_TYPES = [(TEXT, "Text"), (MCQ, "MCQ")]
+
+    exam = models.ForeignKey(Exam, related_name="questions",
+                             on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=10, choices=QUESTION_TYPES)
+    prompt = models.TextField()
+    expected_answer = models.TextField()
+    max_score = models.FloatField(default=1)
+
+    def __str__(self):
+        return self.prompt[:50]
